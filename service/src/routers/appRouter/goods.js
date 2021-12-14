14d2e5f5485bf4c05253d4524f70d57e79bea472
wangyuliang <wangyuliang@nankor.cn>
2021-12-14 17:57:13 +0800

    nk首次提交测试

--- service/src/routers/appRouter/goods.js
+++ service/src/routers/appRouter/goods.js
@@ -3,7 +3,6 @@ const Router = require("koa-router")
 const { uploadImg } = require("../../controller/goodsController")
 const { login, register, updatePass } = require("../../controller/userController")
 const { auth, checkIsAdmin } = require("../../middleware/auth/auth")
-const { checkUserInfoIsNull, checkUserIsExist, encryptPassword, checkLoginInfo } = require("../../middleware/user/userMiddleware")
 
 // 实例化对象,并添加前缀
 const router = new Router({ prefix: "/goods" })
