093cfbc4b3027c701f62ea377826beab70860d62
wangyl <1012719865@qq.com>
2022-01-08 18:22:29 +0800

    添加eslint,prettier,配置

--- admin/src/components/HelloWorld.vue
+++ admin/src/components/HelloWorld.vue
@@ -1,25 +1,39 @@
 <script setup lang="ts">
-import { ref } from "vue"
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
+defineEmits(["changeEvent"]);
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
   <p>
     Recommended IDE setup:
     <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
     +
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
   </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
+    See
+    <code>README.md</code>
+    for more information.
+  </p>
+  <p>
+    <a
+      href="https://vitejs.dev/guide/features.html"
+      target="_blank"
+      tar2get="_blank"
+      tar3get="_bla1nk"
+      ta1rget="_bla2nk"
+      tar4get="_bl4a3nk"
+      tar4g2et="_blank"
+      tar3g2et="_bla3nk"
+      targ2et="_bla4nk"
+    >
+      Vite Docs
+    </a>
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
@@ -27,7 +41,8 @@ const count = ref(0)
   <button type="button" @click="count++">count is: {{ count }}</button>
   <p>
     Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
+    <code>components/HelloWorld.vue</code>
+    to test hot module replacement.
   </p>
 </template>
 
