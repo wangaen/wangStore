b62982b4b110bbe8715d236f905b3d0c5c6df643
wangyuliang <wangyuliang@nankor.cn>
2022-04-11 15:13:42 +0800

    解决因eslint导致使用路径别名、import/export的报红

--- admin/.eslintrc.js
+++ admin/.eslintrc.js
@@ -10,20 +10,6 @@ module.exports = {
     'airbnb-base',
     'plugin:prettier/recommended', // 避免与 prettier 冲突
   ],
-  settings: {
-    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-    'import/resolver': {
-      typescript: {
-        directory: './tsconfig.json',
-      },
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
-  },
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
@@ -33,17 +19,9 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'no-debugger': 'off',
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-        mjs: 'never',
-      },
-    ],
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/no-absolute-path': 'off',
   },
 };
