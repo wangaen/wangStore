17b0d6c241482abda8ab6f31a02e640dbebf2608
wangyuliang <wangyuliang@nankor.cn>
2022-04-12 10:36:30 +0800

    简单测试pinia完成

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
+import userStore from '@/store/modules/user';
 
 const router = useRouter();
 function goRegister() {
@@ -9,6 +10,14 @@ function goRegister() {
 
 <template>
   <div>
+    <p>姓名：{{ userStore.name }}</p>
+    <p>年龄：{{ userStore.age }}</p>
+    <p>性别：{{ userStore.sex }}</p>
+    <p>用户信息：{{ userStore.getUserInfo }}</p>
+    <p>
+      <button @click="userStore.updateName('李四')">修改用户信息</button>
+    </p>
+    <hr />
     <h1>登录页面</h1>
     <p>
       <button @click="goRegister">去注册</button>
